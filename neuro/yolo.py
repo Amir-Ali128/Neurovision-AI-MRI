14eb099d4c234df1df1e980063e1884d7b509589
Amir-Ali128 <ecanali2001@hotmail.com>
2026-02-20 19:12:06 +0300

    Update yolo.py

--- neuro/yolo.py
+++ neuro/yolo.py
@@ -1,15 +1,26 @@
 
 from ultralytics import YOLO
 
-model = YOLO("yolov10n.pt")
+model = None
+
+def get_model():
+    global model
+    if model is None:
+        model = YOLO("yolov10n.pt")
+    return model
 
 def detect(image_path):
+    model = get_model()
+
     results = model(image_path)
+
     detections = []
+
     for r in results:
         for box in r.boxes:
             detections.append({
                 "class": model.names[int(box.cls)],
                 "confidence": float(box.conf)
             })
+
     return detections, results
