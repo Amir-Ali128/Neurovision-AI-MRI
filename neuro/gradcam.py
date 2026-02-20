50c4e82716ca774dc0c642e289d2136c721c18e7
Amir-Ali128 <ecanali2001@hotmail.com>
2026-02-20 22:19:52 +0300

    Update gradcam.py

--- neuro/gradcam.py
+++ neuro/gradcam.py
@@ -1,65 +1,51 @@
 import torch
 import cv2
 import numpy as np
-from ultralytics import YOLO
 
 class GradCAM:
+
     def __init__(self):
-        self.model = YOLO("yolov8n.pt")
-        self.model.model.eval()
 
-        # gradient açık olacak
-        for param in self.model.model.parameters():
+        from neuro.clip_model import model
+
+        self.model = model
+
+        self.model.eval()
+
+        for param in self.model.parameters():
             param.requires_grad = True
-
-        self.activations = None
-        self.gradients = None
-
-        target_layer = self.model.model.model[-2]
-
-        target_layer.register_forward_hook(self.save_activation)
-        target_layer.register_backward_hook(self.save_gradient)
-
-    def save_activation(self, module, input, output):
-        self.activations = output
-
-    def save_gradient(self, module, grad_input, grad_output):
-        self.gradients = grad_output[0]
 
     def generate(self, image_path):
 
         img = cv2.imread(image_path)
-        img_resized = cv2.resize(img, (640,640))
-        img_tensor = torch.from_numpy(img_resized).permute(2,0,1).float().unsqueeze(0)
-        img_tensor.requires_grad = True
+        img = cv2.resize(img, (224,224))
 
-        results = self.model.model(img_tensor)
+        tensor = torch.tensor(img).permute(2,0,1).float().unsqueeze(0)
 
-        score = results[0].mean()
+        tensor.requires_grad = True
 
-        self.model.model.zero_grad()
-        score.backward()
+        output = self.model(tensor)
 
-        gradients = self.gradients[0].cpu().detach().numpy()
-        activations = self.activations[0].cpu().detach().numpy()
+        loss = output.mean()
 
-        weights = np.mean(gradients, axis=(1,2))
+        self.model.zero_grad()
 
-        cam = np.zeros(activations.shape[1:], dtype=np.float32)
+        loss.backward()
 
-        for i, w in enumerate(weights):
-            cam += w * activations[i]
+        grad = tensor.grad[0].permute(1,2,0).detach().numpy()
 
-        cam = np.maximum(cam, 0)
-        cam = cv2.resize(cam, (img.shape[1], img.shape[0]))
-        cam = cam - cam.min()
-        cam = cam / cam.max()
+        heatmap = np.mean(grad, axis=2)
 
-        heatmap = cv2.applyColorMap(np.uint8(255 * cam), cv2.COLORMAP_JET)
+        heatmap = np.maximum(heatmap,0)
 
-        overlay = heatmap * 0.4 + img * 0.6
+        heatmap /= heatmap.max() + 1e-8
 
-        output_path = image_path.replace(".jpg", "_gradcam.jpg")
-        cv2.imwrite(output_path, overlay)
+        heatmap = np.uint8(255 * heatmap)
 
-        return output_path
+        heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+
+        path = "static/heatmap.png"
+
+        cv2.imwrite(path, heatmap)
+
+        return path
